d0117f98e29fcc50d67b9b101038b9917e3ef911
lokki <44494883+luckylokki@users.noreply.github.com>
2022-07-28 10:40:05 +0300

    new commit

--- Django/urls.py
+++ Django/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from Django.views import password_gen, check_password, home, article
 from django.urls import path
 from django.contrib import admin
